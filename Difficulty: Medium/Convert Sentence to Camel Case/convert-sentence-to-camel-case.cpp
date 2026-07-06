61225ece73f30cda5e0e4f155d98499383b1264a
Alpesh6353 <alphesrathod098@gmail.com>
2026-07-06 08:35:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Convert Sentence to Camel Case/convert-sentence-to-camel-case.cpp
+++ Difficulty: Medium/Convert Sentence to Camel Case/convert-sentence-to-camel-case.cpp
@@ -5,27 +5,19 @@ class Solution {
         // code here
         int n = s.length();
         string ans = "";
-        string a = "";
         int i = 0;
-        bool flag1 = false;
-        if(s[0]==' '){  // checking that is first charaacter is letter or space
-            flag1 = true;
-        }
         bool flag = true;
         while(i<n){
             while(i<n && s[i] == ' ') i++;
             if(i>=n) break;
             while(i<n && s[i] != ' '){
-                if(flag1==false) a += s[i];//if first character is letter than do not make it capital
-                else if(flag==true) a += (s[i]-32); // Making capital letter after every space
-                else a += s[i];
+                if(ans == "") ans += s[i];//if first character is letter than do not make it capital
+                else if(flag==true) ans += (s[i]-32); // Making capital letter after every space
+                else ans += s[i];
                 i++;
                 flag = false;
             }
             flag = true;
-            flag1 = true;
-            ans += a;
-            a = "";
         }
         return ans;
     }
